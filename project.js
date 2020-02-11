ce73d351b0a85f121782e0dd987b59bf53e7a391
Andrei the Great <AndreiZarumianiuk@gmail.com>
2020-02-11 20:57:21 +0200

    get coins to the screen

--- project.js
+++ project.js
@@ -1,13 +1,51 @@
 const state = {
     allTheCoins:[],
+    columCounter:1,
 }
-function main(){
 
+function main(){
+getAllCoins();
+state.allTheCoins.forEach((coin)=>{
+rendCoin(coin.name,coin.symbol);
+})
 };
 main();
 
 
 //call all the coins
-
+function getAllCoins(){
+    $.ajax({
+        type: "GET",
+        url: "https://api.coingecko.com/api/v3/coins/list",
+        async : false,
+        success: function (response) {
+            for(let i=0;i<100;i++){
+                state.allTheCoins.push(response[i]);
+            }
+            
+        }
+    });
+}
 
 // rend all the coins
+function rendCoin(coinName,symbol){
+    if(state.columCounter<4){
+    $(`#col-${state.columCounter}`).append(` <div class="coin" >
+    <h2>${symbol}</h2>
+    <p>${coinName}</p>
+    <button id="moreInfo" >More info</button>
+    <input type="checkbox" checked data-toggle="toggle">
+    </div>
+    `);
+    }else{
+        state.columCounter= 1;
+        $(`#col-${state.columCounter}`).append(`<div class="coin" >
+        <h2>${symbol}</h2>
+        <p>${coinName}</p>
+        <button id="moreInfo" >More info</button>
+        <input type="checkbox" checked data-toggle="toggle">
+        </div>
+        `); 
+    }
+    state.columCounter++
+}
